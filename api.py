a9b4cabbe33dcec64c3cd881f5d82e5b6e710739
medori9999 <ohooong@gmail.com>
2026-02-23 12:35:48 +0900

    ,

--- api.py
+++ api.py
@@ -23,6 +23,7 @@ origins = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://white-sea-xxxx.azurestaticapps.net",
 ]
 app.add_middleware(
     CORSMiddleware,
